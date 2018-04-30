1d563c3346b0ea9f937742404c07b079d30c803e
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-30 18:36:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   array = ["kiwi"]
   string = array.join
+  string = array.join(" and ")
   string
 
-  array1 = ["kiwi", "durian"]
-  string1 = array1.join(" and ")
-  string1
 end
